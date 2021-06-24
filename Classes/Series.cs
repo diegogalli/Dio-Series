5530baa3da8e1c47016f90a28c3a73f70c7a479b
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-06-24 14:45:00 -0300

    Projeto Series

---
+++ Classes/Series.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Dio.Series
+{
+    public class Series : EntidadeBase
+    {
+        // Atributos
+
+        private Genero Genero {get; set;}
+        private string Titulo {get; set;}
+        private string Descricao {get; set;}
+        private int Ano {get; set;}
+
+        // Métodos
+
+        public Series (int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id= id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            // Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newLine?view=retorno-3.1
+
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        internal int retornaId()
+        {
+            return this.Id;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dio.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Series meuObjeto = new Series();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
