c3a1866ceaa997c76f6965e1c5a8d9886ca1cf65
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-06-28 14:31:16 -0300

    Atualizando

--- Classes/Series.cs
+++ Classes/Series.cs
@@ -10,6 +10,7 @@ namespace Dio.Series
         private string Titulo {get; set;}
         private string Descricao {get; set;}
         private int Ano {get; set;}
+        private bool Excluido {get; set}
 
         // Métodos
 
@@ -20,6 +21,7 @@ namespace Dio.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -42,6 +44,12 @@ namespace Dio.Series
         internal int retornaId()
         {
             return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,79 @@ namespace Dio.Series
 {
     class Program
     {
+        static SerieRepositorio repositorio = new SerieRepositorio();
         static void Main(string[] args)
         {
-            Series meuObjeto = new Series();
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSerie();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        AtualizarSerie();
+                        break;
+                    case "4":
+                        ExcluirSerie();
+                        break;
+                    case "5":
+                        VisualizarSerie();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();                    
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar o Gallifix.");
+            Console.ReadLine();         
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar Séries");
+            
+            var lista = repositorio.Lista(); 
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série Cadastrada.");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1}", serie.retornaId(), serie.retornaTitulo());
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Gallifix ao seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada: ")
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
